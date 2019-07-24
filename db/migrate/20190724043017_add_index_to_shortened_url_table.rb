3a98636a40524654c9272c4d1d8d0e36868d6252
Spencer <spenjlee@gmail.com>
2019-07-23 21:38:00 -0700

    Add index to shortened urls table

---
+++ db/migrate/20190724043017_add_index_to_shortened_url_table.rb
@@ -0,0 +1,7 @@
+class AddIndexToShortenedUrlTable < ActiveRecord::Migration[5.2]
+	def change
+		add_index :shortened_urls, :user_id
+		add_index :shortened_urls, :long_url, unique: true
+		add_index :shortened_urls, :short_url, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_042257) do
+ActiveRecord::Schema.define(version: 2019_07_24_043017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 2019_07_24_042257) do
     t.string "long_url", null: false
     t.string "short_url", null: false
     t.integer "user_id", null: false
+    t.index ["long_url"], name: "index_shortened_urls_on_long_url", unique: true
+    t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
+    t.index ["user_id"], name: "index_shortened_urls_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
