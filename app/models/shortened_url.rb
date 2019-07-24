3636d8032db0ae7dac33e2d1e5c00a991d90f296
Spencer <spenjlee@gmail.com>
2019-07-23 22:14:05 -0700

    Add class method to return unused random code

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -2,4 +2,10 @@ class ShortenedUrl < ApplicationRecord
 	validates :long_url, presence: true, uniqueness: true
 	validates :short_url, presence: true, uniqueness: true
 	validates :user_id, presence: true
+
+	def self.random_code
+		code = SecureRandom.urlsafe_base64
+		code = SecureRandom.urlsafe_base64 if ShortenedUrl.exists?(short_url: code)
+		code
+	end
 end
