6b058022bddb7832bdb034d68eb788a26c0dec02
Spencer <spenjlee@gmail.com>
2019-07-24 08:06:52 -0700

    Add visits association for User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
 		primary_key: :id,
 		foreign_key: :user_id,
 		class_name:  :ShortenedUrl
+
+	has_many :visits,
+		primary_key: :id,
+		foreign_key: :user_id,
+		class_name:  :Visit
 end
