cd23451558c714f2d81601665d637ee4da0082c1
Spencer <spenjlee@gmail.com>
2019-07-23 20:41:42 -0700

    Add index to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_022959) do
+ActiveRecord::Schema.define(version: 2019_07_24_030803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
