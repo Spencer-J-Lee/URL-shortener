7b683761e114d1640ec7c809d9f76ebf53e5f75f
Spencer <spenjlee@gmail.com>
2019-07-24 07:56:31 -0700

    Remove comment

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,9 +2,6 @@ class Visit < ApplicationRecord
 	validates :user_id, presence: true
 	validates :short_url_id, presence: true
 
-	# Visit::record_visit!(user, shortened_url) 
-	# that will create a Visit object recording a visit from a User to the given ShortenedUrl.
-
 	def self.record_visit!(user, shortened_url)
 		ops_hash = { user_id: user.id, short_url_id: shortened_url.id }
 		Visit.create!(ops_hash)
