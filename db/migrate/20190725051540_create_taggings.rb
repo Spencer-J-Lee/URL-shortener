c280de1a62581c08dec1cde4ffaf861c7f2f34b7
Spencer <spenjlee@gmail.com>
2019-07-24 22:16:56 -0700

    Add tagtopics and taggings tables

---
+++ db/migrate/20190725051540_create_taggings.rb
@@ -0,0 +1,8 @@
+class CreateTaggings < ActiveRecord::Migration[5.2]
+  def change
+		create_table :taggings do |t|
+			t.integer :tag_topic_id, null: false
+			t.integer :short_url_id, null: false
+    end
+  end
+end
