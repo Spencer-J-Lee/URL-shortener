e641ccb4bc263a0afdf896e2bfd767ab451d1e45
Spencer <spenjlee@gmail.com>
2019-07-24 22:19:38 -0700

    Migrate tagtopics and taggings table to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_235747) do
+ActiveRecord::Schema.define(version: 2019_07_25_051540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2019_07_24_235747) do
     t.index ["long_url"], name: "index_shortened_urls_on_long_url"
     t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
     t.index ["user_id"], name: "index_shortened_urls_on_user_id"
+  end
+
+  create_table "tag_topics", force: :cascade do |t|
+    t.string "topic", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_topic_id", null: false
+    t.integer "short_url_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
