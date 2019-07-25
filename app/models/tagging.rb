af1119b8fe0fbc77d92c470ee3327653850c7e74
Spencer <spenjlee@gmail.com>
2019-07-24 22:38:27 -0700

    Add short url and topic association for Tagging class

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,3 +1,13 @@
 class Tagging < ApplicationRecord
 	validates :tag_topic_id, :short_url_id, presence: true
+
+	has_one :short_url,
+		primary_key: :short_url_id,
+		foreign_key: :id,
+		class_name:  :ShortenedUrl
+
+	has_one :topic,
+		primary_key: :tag_topic_id,
+		foreign_key: :id,
+		class_name:  :TagTopic
 end
