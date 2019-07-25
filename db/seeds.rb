67cc8ace91fd5c39aefa1e5616ee9c3119cb2dec
Spencer <spenjlee@gmail.com>
2019-07-24 17:05:25 -0700

    Seed the database with test entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating users"
+User.destroy_all
+user1 = User.create(email: 'test_email')
+user2 = User.create(email: 'another_test_email')
+
+puts "Creating shortened urls"
+ShortenedUrl.destroy_all
+url1 = ShortenedUrl.create_from_user(user1)
+url2 = ShortenedUrl.create_from_user(user1)
+
+puts "Creating visits"
+Visit.destroy_all
+visit1 = Visit.record_visit!(user1, url1)
+visit2 = Visit.record_visit!(user1, url2)
+visit3 = Visit.record_visit!(user2, url1)
