3e9a451b2bbd4dff7e7f737f33418836ca2587b2
Spencer <spenjlee@gmail.com>
2019-07-24 22:50:46 -0700

    Add tagged urls association to TagTopic class

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -5,4 +5,9 @@ class TagTopic < ApplicationRecord
 		primary_key: :id,
 		foreign_key: :tag_topic_id,
 		class_name:  :Tagging
+
+	has_many :tagged_urls,
+		-> { distinct },
+		through: :taggings,
+		source:  :short_url
 end
